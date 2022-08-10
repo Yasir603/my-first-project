9bfb0e7983d64770ba281bc778403b22c81186cc
Code for business <102666528+Yasir603@users.noreply.github.com>
2022-08-10 11:43:08 +0500

    Add files via upload

--- app/agents/RegisterUser.php
+++ app/agents/RegisterUser.php
@@ -1,10 +1,70 @@
 <?php
+use carbon/Carbon;
+use Ramsey\Uuid\Uuid;
 
 class RegisterUser extends ElectroApi {
 
+    const USERNAME = 'username';
+    const EMAIL = 'email';
+    const PASSWORD = 'password';
+    const AVATAR = 'avatar';
+
+
+    protected function onAssemble(){
+      $this->killWithBadRequestExceptionIfTextualParamIsMissing(self::USERNAME);
+      $this->killWithBadRequestExceptionIfTextualParamIsMissing(self::EMAIL);
+      $this->killWithBadRequestExceptionIfTextualParamIsMissing(self::PASSWORD);
+      $this->killWithBadRequestExceptionIfMultipartParamIsMissing(self::AVATAR);
+    }
+
     protected function onDevise() {
+
+      $generatedName = "";
+      $isImageSaved = ImageUploader::withSrc($_FILES[self::AVATAR]['tmp_name'])
+          ->destinationDir($this->getUserAvatarImageDirPath())
+          ->generateUniqueName($_FILES[self::AVATAR]['name'])
+          ->mapGeneratedName($generatedName)
+          ->compressQuality(75)
+          ->save();
+
+      if (!$isImageSaved) {
+          $this->killFailureWithMsg('failed_to_save_image');
+      }
+
+      $this->killCustomFailureIfAnyHaveSome(
+          'user_with_this_email_already_exist',
+          null,
+          $this->getAppDB()->getUserDao()->getUserWithEmail($_POST[self::EMAIL])
+      );
+
+
+      $register_time = Carbon::now();
+
+      $userEntity = $this->killFailureIfNullElseGetUserEntity(
+          $this->getAppDB()->getUserDao()->insertUser(
+              new UserEntity(
+                  Uuid::uuid4()->toString(),
+                  $_POST[self::USERNAME],
+                  $_POST[self::EMAIL],
+                  $_POST[self::PASSWORD],
+                  $generatedName
+
+                  $register_time,
+                  $register_time
+              )
+          ), null ,
+          "failed_to_insert_user_entity"
+      );
+
+
+
         $this->resSendOK([
-            'eevee' => 'Hi i\'m RegisterUser agent.'
+          'user' => [
+            'uid' => $userEntity->getUid(),
+            'username' => $userEntity->getUsername(),
+            'email' => $userEntity->getEmail(),
+            'avatar' => $this->createLinkForUserAvatarImage($userAvatarEntity->getAvatar())
+         ]
         ]);
     }
 }
