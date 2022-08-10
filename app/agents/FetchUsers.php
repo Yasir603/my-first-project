deb2ca73230630e59e02f71e669683ecea3e23a4
yasir mubeen <yasirmubeen603@gmail.com>
2022-08-10 15:52:03 +0500

    fetch all users

--- app/agents/FetchUsers.php
+++ app/agents/FetchUsers.php
@@ -3,8 +3,28 @@
 class FetchUsers extends ElectroApi {
 
     protected function onDevise() {
+      $userEntities = $this->getAppDB()->getUserDao()->getAllUser();
+         $users = [];
+
+         foreach ($userEntities as $userEntity) {
+
+           array_push($users,/** @_ */
+             [
+                 'uid' => $userEntity->getUid(),
+                 'username' => $userEntity->getUsername(),
+                 'email' => $userEntity->getEmail(),
+                 'password' => $userEntity->getPassword(),
+                 'image' => $this->createLinkForUserAvatarImage($userEntity->getAvatar()),
+                 'registration_time' => $userEntity->getCreatedAt()
+             ]);
+         }
+
+
+
+
+
         $this->resSendOK([
-            'eevee' => 'Hi i\'m FetchUsers agent.'
+            'users' => $users
         ]);
     }
 }
