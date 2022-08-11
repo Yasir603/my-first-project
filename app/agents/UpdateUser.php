1be34fac877dc95721b44e914d1420391bae1905
yasir mubeen <yasirmubeen603@gmail.com>
2022-08-11 12:00:10 +0500

    updateuser

--- app/agents/UpdateUser.php
+++ app/agents/UpdateUser.php
@@ -1,7 +1,22 @@
 <?php
 
+use Carbon\Carbon;
 class UpdateUser extends ElectroApi {
 
+    const USER_UID ='uid';
+    const USERNAME = 'username';
+    const EMAIL = 'email';
+    const PASSWORD = 'password';
+    const AVATAR = 'avatar';
+
+
+    protected function onAssemble() {
+    $this->killWithBadRequestExceptionIfTextualParamIsMissing(self::USER_UID);
+    $this->killWithBadRequestExceptionIfTextualParamIsMissing(self::USERNAME);
+    $this->killWithBadRequestExceptionIfTextualParamIsMissing(self::EMAIL);
+    $this->killWithBadRequestExceptionIfTextualParamIsMissing(self::PASSWORD);
+    $this->killWithBadRequestExceptionIfMultipartParamIsMissing(self::AVATAR);
+    }
     protected function onDevise() {
         $this->resSendOK([
             'eevee' => 'Hi i\'m UpdateUser agent.'
