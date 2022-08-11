15862547592475e120d3e0e1b18cd5fa2c428f1a
yasir mubeen <yasirmubeen603@gmail.com>
2022-08-11 11:37:42 +0500

    user deleted

--- app/agents/DeleteUser.php
+++ app/agents/DeleteUser.php
@@ -2,9 +2,28 @@
 
 class DeleteUser extends ElectroApi {
 
+
+    const USER_UID ='uid';
+
+    protected function onAssemble() {
+
+    $this->killWithBadRequestExceptionIfTextualParamIsMissing(self::USER_UID);
+
+    }
+
     protected function onDevise() {
+        $DeleteUser = $this->killCompromisedIfNullElseGetUserWithUid(
+        $this->getAppDB()->getUserDao()->getUserWithUid($_POST[self::USER_UID]),
+        null ,
+        "no_user_found"
+    );
+
+     $this->getAppDB()->getUserDao()->deleteUser($DeleteUser);
+
+
+
         $this->resSendOK([
-            'eevee' => 'Hi i\'m DeleteUser agent.'
+            'user' => 'user deleted successfully.'
         ]);
     }
 }
